4002e67207753137eb4af0c8ae5bd37279196c35
Parth Sane <laerdevstudios@gmail.com>
2014-04-23 00:08:37 +0530

    Service Discovery Now Works
    Big improvement as far as service discovery goes. UI has to be designed.

--- gen/code/laerstudios/openairplay/R.java
+++ gen/code/laerstudios/openairplay/R.java
@@ -22,18 +22,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int lightbulb=0x7f020001;
-        public static final int lightbulb_off=0x7f020002;
-        public static final int openairplay=0x7f020003;
+        public static final int applelogo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int lightbulb=0x7f020002;
+        public static final int lightbulb_off=0x7f020003;
+        public static final int openairplay=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
+        public static final int applelogo=0x7f080003;
         public static final int fragment_firstpane=0x7f080001;
         public static final int fragment_secondpane=0x7f080002;
-        public static final int icon=0x7f080003;
         public static final int slidingpanelayout=0x7f080000;
-        public static final int text=0x7f080004;
+        public static final int text=0x7f080005;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
